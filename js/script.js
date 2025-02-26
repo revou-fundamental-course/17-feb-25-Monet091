0ba935a63d05e68990333a20af85c56ddfe65f5c
Aditya Faiz Syaban <neadynamo091@gmail.com>
2025-02-26 23:36:41 +0700

    tambah responsivitas dan banner slideshow pada halaman utama

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-//test
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
+
+showSlide(currentSlide);
